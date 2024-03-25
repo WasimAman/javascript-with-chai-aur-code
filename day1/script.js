bae41d42cb01d48199788b31381ec0e8a2d3416f
WasimAman <wasimaman65@gmail.com>
2024-03-25 10:22:37 +0530

    new changes

--- day1/script.js
+++ day1/script.js
@@ -39,3 +39,36 @@ console.log(typeof BigInt(12)); //bigInt
 console.log(typeof {name:"wasim"}); //object
 console.log(typeof true);   //boolean
 
+
+// type conversion
+// in Number
+console.log("Number Conversion\n");
+console.log(Number("12"));  //12 in number (int)
+console.log(Number(null));  // 0
+console.log(Number(undefined)); //NaN (not a number)
+console.log(Number(true));  //1
+console.log(Number(false)); //0
+console.log(Number({name : "wasim"}));  // Nan (not a number)
+
+
+// in String 
+console.log("String conversion\n");
+console.log(String(12));    //12
+console.log(String(null));  //null
+console.log(String(undefined)); //undefined
+console.log(String(true));  //true 
+console.log(String(false)); //false
+console.log(String({name : "wasim"}));  //[object object]
+
+console.log(typeof String(12)); //string
+console.log(typeof String(null));   //string
+console.log(typeof String(undefined));  //string
+console.log(typeof String(true));   //string
+console.log(typeof String(false));  //string
+console.log(typeof String({name : "wasim"}));   //string
+
+/*
+    we can also convert boolean to number,string etc..
+
+    imp:- we can convert from any data type to any data type.
+*/
