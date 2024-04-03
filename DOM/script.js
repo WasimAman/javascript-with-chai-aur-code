f22852da079c355fc349375a990c98e4631e5b3d
WasimAman <wasimaman65@gmail.com>
2024-04-03 12:00:27 +0530

    Dom Manipulation

---
+++ DOM/script.js
@@ -0,0 +1,8 @@
+document.getElementById("myPara");
+// this will give a Element whose id is "myPara"
+
+document.getElementsByTagName("h1");
+// this will give all h1 tag
+
+document.getElementsByClassName("heading");
+// this will return a html-collection whose class will be heading
